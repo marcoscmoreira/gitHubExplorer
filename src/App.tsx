bf64e946ad8b0361e4cd35beb973dfd58489a088
marcoscmoreira <marcos_coelho2011@hotmail.com>
2020-11-11 23:56:27 +0000

    gitHubExplorer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
+/* eslint-disable no-unused-expressions */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/global';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
+);
 
 export default App;
